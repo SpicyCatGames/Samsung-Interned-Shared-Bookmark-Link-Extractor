40ca1d9849cf5d97b586142292e7c3418d83a5da
SpicyCatGames <61442059+SpicyCatGames@users.noreply.github.com>
2021-02-03 07:34:06 +0600

    Data changed at source and only links will remain

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BookmarkOrganizer
 {
@@ -11,31 +12,25 @@ namespace BookmarkOrganizer
     {
         static void Main(string[] args)
         {
-            string sourceDirectory = @"C:\Users\Md Sadman Chowdhury\source\repos\BookmarkOrganizer\Data\Source";
-            string archiveDirectory = @"C:\Users\Md Sadman Chowdhury\source\repos\BookmarkOrganizer\Data\Archive\";
+            string sourceDirectory = @"C:\Users\Md Sadman Chowdhury\source\repos\BookmarkOrganizer\Data\Source\";
+            String[] files =  Directory.GetFiles(sourceDirectory, "*.*", SearchOption.AllDirectories);
 
             try
             {
-                var txtFiles = Directory.EnumerateFiles(sourceDirectory, "*.txt");
                 int x = 1;
-                foreach (string currentFile in txtFiles)
+                foreach (string currentFile in files)
                 {
                     string text = System.IO.File.ReadAllText(@currentFile);
                     string[] fileNames = currentFile.Split('\\');
                     string fileName = fileNames[fileNames.Length - 1];
-                    string names = "";
                     string values = "";
-                    
+
                     string[] subs = text.Split('\n');
                     for (int y = 0; y < subs.Length; y++)
                     {
                         if (y == 0 || y % 2 == 0)
                         {
-                            names += subs[y];
-                            if (y + 2 < subs.Length)
-                            {
-                                names += '\n';
-                            }
+
                         }
                         else
                         {
@@ -47,8 +42,7 @@ namespace BookmarkOrganizer
                         }
                     }
 
-                    System.IO.File.WriteAllText(archiveDirectory + fileName.Substring(0, fileName.Length - 4) + "name.txt", names);
-                    System.IO.File.WriteAllText(archiveDirectory + fileName.Substring(0, fileName.Length - 4) + "value.txt", values);
+                    System.IO.File.WriteAllText(@currentFile, values);
 
                     x++;
                 }
